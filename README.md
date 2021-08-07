cc2e6dc4ed3757118761d7292e4cf3a33d22ee18
sunil-sharma-999 <84917190+sunil-sharma-999@users.noreply.github.com>
2021-08-07 20:55:50 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,55 @@
+# Live Site
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+# Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+- Sass
+
+### Useful resources
+
+- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@yourusername](https://www.twitter.com/yourusername)
---
+++ app.js
@@ -0,0 +1,111 @@
+// general variables
+const theme = document.querySelector('#toggle');
+const screen = document.querySelector('.screen');
+let used = false;
+
+// theme toggle
+theme.addEventListener('click', () => {
+  themeToggle();
+});
+const themeToggle = () => {
+  const body = document.body;
+  if (theme.value === '1') {
+    body.classList = 'theme1';
+  } else if (theme.value === '2') {
+    body.classList = 'theme2';
+  } else if (theme.value === '3') {
+    body.classList = 'theme3';
+  }
+};
+
+// physical keypad
+document.addEventListener('keydown', (e) => {
+  if (
+    (e.key >= 0 && e.key <= 9) ||
+    e.key === '+' ||
+    e.key === '-' ||
+    e.key === '/' ||
+    e.key === '*' ||
+    e.key === '.'
+  ) {
+    keyPressed(e.key);
+  } else if (e.key === 'Enter') {
+    calc();
+  } else if (e.key === 'Backspace') {
+    clearDigit();
+  } else if (e.key === 'Delete') {
+    clearScreen();
+  }
+});
+
+// keypad
+const keyPressed = (val) => {
+  const lastChar = screen.textContent[screen.textContent.length - 1];
+  if (val === '.') {
+    decimalValidation(val, lastChar);
+    return;
+  } else if (val === '+' || val === '-' || val === '*' || val === '/') {
+    used = false;
+    keyValidation(val, lastChar);
+  } else {
+    screen.textContent += `${val}`;
+  }
+  fontSize();
+};
+
+const calc = () => {
+  const noOperator = screen.textContent.match(/[*+/-]/g);
+  if (!noOperator) {
+    return;
+  } else {
+    const value = eval(screen.textContent);
+    screen.textContent = value.toLocaleString('en-US');
+  }
+  fontSize();
+};
+
+const clearDigit = () => {
+  screen.textContent = screen.textContent.substring(
+    0,
+    screen.textContent.length - 1
+  );
+  fontSize();
+};
+const clearScreen = () => {
+  screen.textContent = '';
+};
+
+// font size
+const fontSize = () => {
+  if ((screen.textLength < 11) & (screen.textLength > 0)) {
+    screen.style.fontSize = '2.5rem';
+  } else if ((screen.textLength > 10) & (screen.textLength <= 42)) {
+    screen.style.fontSize = '1.2rem';
+  } else if (screen.textLength > 42) {
+    screen.style.fontSize = '0.5rem';
+  }
+};
+
+// key validate
+const keyValidation = (val, lastChar) => {
+  if (
+    lastChar === '+' ||
+    lastChar === '-' ||
+    lastChar === '*' ||
+    lastChar === '/' ||
+    screen.textLength === 0
+  ) {
+    return;
+  } else {
+    screen.textContent += `${val}`;
+  }
+};
+
+const decimalValidation = (val, lastChar) => {
+  if (used === false && lastChar !== '.') {
+    screen.textContent += `${val}`;
+    used = true;
+  } else {
+    return;
+  }
+};
